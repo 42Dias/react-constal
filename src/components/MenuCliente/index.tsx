caeaae9ea9c9489c01405be42c37d73190e0fb34
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-11-16 15:25:16 -0300

    error screen

---
+++ src/components/MenuCliente/index.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import { FiChevronRight } from "react-icons/fi";
+import {  FlexLink, Container } from "./styles";
+
+export default function MenuCliente() {
+  return (
+    <>
+      <Container>
+        <FlexLink>
+          <Link to="/produtos">
+            Madeira <FiChevronRight />
+          </Link>
+          <Link to="/produtos">
+            Tinta <FiChevronRight />
+          </Link>
+          <Link to="/produtos">
+            Portas e Janelas <FiChevronRight />
+          </Link>
+          <Link to="/produtos">
+            Telha <FiChevronRight />
+          </Link>
+          <Link to="/produtos">
+            Pisos <FiChevronRight />
+          </Link>
+          <Link to="/produtos">
+            Ferramentas <FiChevronRight />
+          </Link>
+        </FlexLink>
+      </Container>
+    </>
+  );
+}
