f77c16cdf446a586a8a567c39001dbbc74aa6dc6
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-11-16 15:28:38 -0300

    error screen

---
+++ src/pages/Dashboard/PersonalData/index.tsx
@@ -0,0 +1,99 @@
+// import React from "react";
+// import Modal from "react-modal";
+import { Link } from "react-router-dom";
+import Header from "../../../components/Header";
+import MenuAdm from "../../../components/MenuAdm";
+import {
+  // CardProfile,
+  // CardDatas,
+  // Title,
+  CardDatails,
+  CardDatailsContent,
+  ContentDetails,
+} from "./styles";
+
+export default function PersonalData() {
+  return (
+    <>
+      <Header />
+      <MenuAdm />
+      <div className="container">
+        <CardDatails>
+          <h2>Dados da conta</h2>
+          <CardDatailsContent>
+            <ContentDetails>
+              <span>Login:</span>
+              <p>Julia Souza</p>
+            </ContentDetails>
+          </CardDatailsContent>
+
+          <CardDatailsContent>
+            <ContentDetails>
+              <span>Senha:</span>
+              <p>******</p>
+            </ContentDetails>
+            <Link to="">Alterar</Link>
+          </CardDatailsContent>
+        </CardDatails>
+
+        <CardDatails>
+          <h2>Empresa</h2>
+          <CardDatailsContent>
+            <ContentDetails>
+              <span>Marca:</span>
+            </ContentDetails>
+          </CardDatailsContent>
+
+          <CardDatailsContent>
+            <ContentDetails>
+              <span>Razão Social:</span>
+            </ContentDetails>
+          </CardDatailsContent>
+
+          <CardDatailsContent>
+            <ContentDetails>
+              <span>CNPJ:</span>
+            </ContentDetails>
+          </CardDatailsContent>
+
+          <CardDatailsContent>
+            <ContentDetails>
+              <span>Telefone:</span>
+            </ContentDetails>
+          </CardDatailsContent>
+
+          <CardDatailsContent>
+            <ContentDetails>
+              <span>E-mail:</span>
+            </ContentDetails>
+          </CardDatailsContent>
+
+          <CardDatailsContent>
+            <ContentDetails>
+              <span>Website:</span>
+            </ContentDetails>
+          </CardDatailsContent>
+        </CardDatails>
+
+        <CardDatails>
+          <h2>Endereço</h2>
+          <CardDatailsContent>
+            <ContentDetails>
+              <small>
+                Av. xxxxxxxx <br />
+                Referência: XXXXXX <br />
+                CEP: 07355-620 <br />
+                Cidade: SP
+              </small>
+            </ContentDetails>
+            <div className="flex-btn">
+              <Link to="">Alterar</Link>
+              <Link to="">Excluir</Link>
+            </div>
+          </CardDatailsContent>
+          <Link to="">Novo endereço</Link>
+        </CardDatails>
+      </div>
+    </>
+  );
+}
