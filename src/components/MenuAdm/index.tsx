84ce4f08fb6efa5acb7156d0d8efa7f1ee098662
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-11-16 15:22:01 -0300

    error screen

---
+++ src/components/MenuAdm/index.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import { FiChevronRight } from "react-icons/fi";
+import {  FlexLink, Container } from "./styles";
+
+export default function MenuAdm() {
+  return (
+    <>
+      <Container>
+        <FlexLink>
+          <Link to="/meus-produtos">
+            Produtos <FiChevronRight />
+          </Link>
+          <Link to="/promocoes">
+            Promoções <FiChevronRight />
+          </Link>
+          <Link to="/produtos">
+            Vendas <FiChevronRight />
+          </Link>
+          <Link to="/produtos">
+            Perguntas <FiChevronRight />
+          </Link>
+          <Link to="/produtos">
+            Planos <FiChevronRight />
+          </Link>
+          <Link to="/dados-pessoais">
+          Dados Pessoais <FiChevronRight />
+          </Link>
+        </FlexLink>
+      </Container>
+    </>
+  );
+}
