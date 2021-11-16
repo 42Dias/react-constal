8dfce76d66e53f8fd5d3f3f24e836680886fa800
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2021-11-16 10:00:52 -0300

    add prod

--- src/pages/Dashboard/NewProd/index.tsx
+++ src/pages/Dashboard/NewProd/index.tsx
@@ -1,22 +1,49 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 // import { FiChevronRight } from "react-icons/fi";
-import { GridProdsFour, ProdContainerSingle } from "./styles";
+import React from "react";
+import Modal from "react-modal";
+import {
+  GridProdsFour,
+  ProdContainerSingle,
+  ContentNew,
+  ModalContainerVendedor,
+  ModalFlex,
+  ModalContent,
+  ContentFormNew,
+} from "./styles";
 import prodone from "../../../assets/images/prodone.png";
 import prodtwo from "../../../assets/images/prodtwo.png";
 import prodthree from "../../../assets/images/prodthree.png";
 import prodfour from "../../../assets/images/prodfour.png";
+import upload from "../../../assets/images/upload.svg";
 import Header from "../../../components/Header";
 import MenuAdm from "../../../components/MenuAdm";
-import { AiOutlinePlus } from "react-icons/ai";
+import { AiOutlinePlus, AiOutlineClose } from "react-icons/ai";
+// import { Link } from "react-router-dom";
 
 export default function NewProd() {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <>
       <Header />
       <MenuAdm />
       <div className="container">
-        <h2>Meus produtos</h2>
-        <span>Adicionar</span>
+        <ContentNew>
+          <h2>Meus produtos</h2>
+          <p onClick={openModal}>Adicionar</p>
+        </ContentNew>
 
         <GridProdsFour>
           <ProdContainerSingle>
@@ -132,6 +159,92 @@ export default function NewProd() {
           </ProdContainerSingle>
         </GridProdsFour>
       </div>
+
+      <ModalContainerVendedor>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+        >
+          <div>
+            <ModalFlex>
+              <AiOutlineClose onClick={closeModal} />
+            </ModalFlex>
+
+            <ModalContent>
+              <img src={upload} alt="" />
+              <h3>Novo produto</h3>
+
+              <ContentFormNew>
+                <label htmlFor="">Nome do produto</label>
+                <input type="text" placeholder="Nome do produto" />
+              </ContentFormNew>
+
+              <ContentFormNew>
+                <label htmlFor="">Descrição</label>
+                <input type="text" placeholder="Descrição" />
+              </ContentFormNew>
+
+              <ContentFormNew>
+                <label htmlFor="">Especificações técnicas</label>
+                <input type="text" placeholder="Especificações técnicas" />
+              </ContentFormNew>
+
+              <ContentFormNew>
+                <label htmlFor="">Especificações técnicas</label>
+                <input type="text" placeholder="Especificações técnicas" />
+              </ContentFormNew>
+
+              <ContentFormNew>
+                <label htmlFor="">Preço</label>
+                <input type="text" placeholder="Preço" />
+              </ContentFormNew>
+
+              <ContentFormNew>
+                <label htmlFor="">Categoria</label>
+                <select>
+                  <option value="">Prato</option>
+                  <option value="">Janelas</option>
+                  <option value="">Madeiras</option>
+                </select>
+              </ContentFormNew>
+
+              <ContentFormNew>
+                <label htmlFor="">Cores disponíveis</label>
+                <select>
+                  <option value="">Verde</option>
+                  <option value="">Preto</option>
+                  <option value="">Amarelo</option>
+                </select>
+              </ContentFormNew>
+
+              <ContentFormNew>
+                <label htmlFor="">Unidades de medida</label>
+                <input type="text" placeholder="Unidades de medida" />
+              </ContentFormNew>
+
+              <ContentFormNew>
+                <label htmlFor="">Prazo de entrega</label>
+                <input type="text" placeholder="Prazo de entrega" />
+              </ContentFormNew>
+
+              <ContentFormNew>
+                <label htmlFor="">Tipo de frete</label>
+                <select>
+                  <option value="">Sedex</option>
+                  <option value="">A combinar</option>
+                  <option value="">Retirar</option>
+                </select>
+              </ContentFormNew>
+
+              <div className="buttonsNew">
+                <a href="">Cancelar</a>
+                <a href="">Adicionar</a>
+              </div>
+            </ModalContent>
+          </div>
+        </Modal>
+      </ModalContainerVendedor>
     </>
   );
 }
--- src/pages/Dashboard/NewProd/styles.ts
+++ src/pages/Dashboard/NewProd/styles.ts
@@ -49,3 +49,114 @@ export const ProdContainerSingle = styled.div`
     }
   }
 `;
+
+export const ContentNew = styled.div`
+  padding: 20px 0;
+  h2 {
+    font-size: 30px;
+  }
+
+  p {
+    margin: 20px 0 !important;
+    width: 182px;
+    height: 33px;
+
+    background: #58A4B0;
+    border-radius: 5px;
+    color: white;
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`
+
+export const ModalContainerVendedor = styled.div`
+  display: flex;
+`;
+
+export const ModalFlex = styled.div`
+  display: flex;
+`;
+
+export const ModalContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: left;
+
+  h3 {
+    padding: 20px 0;
+    font-size: 24px;
+  }
+
+  p {
+    color: #4A4A4A;
+    max-width: 420px;
+    padding: 20px 0;
+  }
+
+  .buttonsNew {
+    display: flex;
+  }
+
+  .buttonsNew a {
+    width: 165px;
+    height: 40px;
+
+    border-radius: 5px;
+    color: black;
+    border: 1px solid rgba(16, 16, 16, 1);
+    text-decoration: none;
+  
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 5px;
+  }
+
+  .buttonsNew a:nth-child(2) {
+    width: 165px;
+    height: 40px;
+
+    background: #101010;
+    border-radius: 5px;
+    color: white;
+    text-decoration: none;
+  
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 5px;
+  }
+`;
+
+export const ContentFormNew = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  input {
+    height: 48px;
+    width: 466px;
+    border-radius: 4px;
+    border: 0;
+    background: #F2F2F2 !important;
+    padding: 0 5px;
+    margin: 10px 0;
+  }
+
+  select {
+    height: 48px;
+    width: 466px;
+    border-radius: 4px;
+    border: 0;
+    background: #F2F2F2 !important;
+    padding: 0 5px;
+    margin: 10px 0;
+  }
+
+  label {
+    margin: 5px 0;
+  }
+` 
