dc7a1e1b1fc5e5537a6f1768cc5739b6d4aa2739
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2021-12-02 15:17:49 -0300

    criada a logica dos favoritos (mostrando na tela)

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -8,24 +8,69 @@ import {
   ContentDetails,
   Title,
 } from "./styles";
+import React, { useState ,useEffect } from "react";
 import prodfav from "../../assets/images/prodfav.png";
+import { Product } from '../../types';
+import { api } from "../../services/api";
+import { formatPrice } from "../../util/format";
 
 export default function Favorites() {
+
+  const [products = [], setProducts] = useState<Product[]>([]);
+
+
+
+// function setFavoritos(favoritos: string[], produtoId: string){//essa função vai para o produto e home
+//     if(favoritos){
+//         favoritos.push(produtoId)
+//     }
+//     else{
+//         localStorage.setItem("favorito", JSON.stringify([produtoId]))
+//     }
+// }
+function loadFavoritos(favoritos: string[]){
+    if(favoritos){
+        favoritos.map(
+            async favorito => {
+                console.log(favorito)
+                const response: Product[] = await api.get(`produto/${favorito}`);
+                setProducts(response)
+            } 
+        )
+    }
+}
+
+  useEffect(() => {
+    
+    async function loadProducts() {
+      const favoritos: string[] = JSON.parse(localStorage.getItem("favoritos") || '[]' );
+      loadFavoritos(favoritos)
+    }
+  loadProducts();
+  }, []);
+  
+
+
+
   return (
     <>
       <Header />
       <div className="container">
         <CardDatails>
           <Title>Favoritos</Title>
-
-          <CardDatailsContent>
-            <ContentDetails>
-              <img src={prodfav} alt="" />
-              <span>Headset Preto</span>
-              <p>R$ 999,99</p>
-            </ContentDetails>
-            <Link to="">Comprar</Link>
-          </CardDatailsContent>
+          {
+            products.map((product: any) => (
+              <CardDatailsContent>
+              <ContentDetails>
+                <img src={prodfav} alt="" />
+                <span>product.nome</span>
+                <p>formatPrice(product.preco)</p>
+              </ContentDetails>
+              <Link to="">Comprar</Link>
+            </CardDatailsContent>  
+            )
+            )
+          }
 
           <CardDatailsContent>
             <ContentDetails>
