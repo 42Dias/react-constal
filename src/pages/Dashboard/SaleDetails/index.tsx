052ec61d3c3009b9469889bd6faaaea8832818ab
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2021-12-15 19:55:45 -0300

    adicionado o pedido específico

--- src/pages/Dashboard/SaleDetails/index.tsx
+++ src/pages/Dashboard/SaleDetails/index.tsx
@@ -4,8 +4,94 @@ import Header from "../../../components/Header";
 import item from "../../../assets/images/prodfav.png";
 import { Link } from "react-router-dom";
 import { Menu } from "../../../components/Menu";
+import { useEffect, useState } from "react";
+import { api } from "../../../services/api";
+import { complement } from "polished";
+import { formatPrice } from "../../../util/format";
 
 export default function SaleDetails() {
+
+  const [pedido, setPedido]=useState<any>();
+
+  const [produto, setProduto]=useState<any>();
+  const [produtoId, setProdutoId]=useState('');
+
+  const [preco, setPreco]=useState('');
+  const [img, setImg]=useState('');
+  const [cpf, setCPF]=useState('');
+  const [phone, setPhone]=useState('');
+  const [logradouro, setLogradouro]=useState('');
+  const [bairro, setBairro]=useState('');
+  const [cep, setCEP]=useState('');
+  const [cidade, setCidade]=useState('');
+  const [estado, setEstado]=useState('');
+  const [valorEntrega, setValorEntrega]=useState('');
+  const [rua, setRua]=useState('');
+  const [numero, setNumero]=useState('');
+  const [complemento, setComplemento]=useState('');
+  const [valorFrete, setValorFrete]=useState(0);
+  const [valorTotal, setValorTotal]=useState(0);
+  const [formaPagamento, setFormaPagamento]=useState(0);
+  const [dataPedido, setDataPedido]=useState('');
+  
+  
+  
+
+  
+
+  const [total, setTotal]=useState('');
+
+
+  
+
+  function getHash() {
+      const hash = window.location.hash.replace(/#\/detalhes-da-venda\//g, '');    
+      // /#\/detalhes-da-venda\//
+      // const hash = window.location.hash.replace(/#\/produto\//g, '');    
+      return hash
+    }
+
+  useEffect(
+    () => {
+      async function loadPedidoDetails() {
+        const pedidoId = getHash()
+        console.log("pedidoId")      
+        console.log(pedidoId)      
+        const response = await api.get(`find-pedido/${pedidoId}`)        
+        console.log(response.data)
+        setPedido(response.data)
+
+        // setProdutoId(pedido.produto.id)
+        // setProduto(pedido.produto)
+        setPreco(response.data.preco)
+        setImg(response.data.img)
+        setCPF(response.data.cpf)
+        setPhone(response.data.telefone)
+        setLogradouro(response.data.logradouro)
+        setBairro(response.data.bairro)
+        setCEP(response.data.cep)
+        setCidade(response.data.cidade)
+        setEstado(response.data.estado)
+        setValorEntrega(response.data.valorEntrega)
+        setRua(response.data.rua)
+        setNumero(response.data.numero)
+        setComplemento(response.data.complemento)
+        setFormaPagamento(response.data.formaPagamento)
+        setValorFrete(response.data.valorFrete)
+        setValorTotal(response.data.valorTotal)
+        setDataPedido(response.data.dataPedido)
+
+        /*
+        NECESSIDADE DE LINKAR O PRODUTO COM O PEDIDO
+        */
+
+      }
+
+      loadPedidoDetails()
+    }
+    ,[])
+
+
   return (
     <>
       <Header />
@@ -26,9 +112,9 @@ export default function SaleDetails() {
             <CardDatailsContent>
               <ContentDetails>
                 <small>
-                  <b>Nome do cliente</b> <br />
-                  CPF: XXXX <br />
-                  Metodo de pagamento: XXXXXX <br />
+                  <b>Nome do cliente: </b> <br />
+                  CPF: {cpf} <br />
+                  Metodo de pagamento: {formaPagamento} <br />
                   Parcelamento: XX
                 </small>
               </ContentDetails>
@@ -38,9 +124,9 @@ export default function SaleDetails() {
               <ContentDetails>
                 <small>
                   <b>Entrega</b> <br />
-                  Endereço de destino: Rua, Número, Bairro, CEP <br />
-                  Complemento: XXXX <br />
-                  Cidade/Estado
+                  Endereço de destino: {/*{rua},*/} {numero}, {bairro}, {cep} <br />
+                  Complemento: {complemento} <br />
+                  {cidade}/{estado}
                 </small>
               </ContentDetails>
             </CardDatailsContent>
@@ -49,10 +135,10 @@ export default function SaleDetails() {
               <ContentDetails>
                 <small>
                   <b>Detalhes</b> <br />
-                  Data de solicitação da compra:  00/00/0000 <br />
-                  Produto R$ 00,00 <br />
-                  Envio R$ 00,00 <br />
-                  <b>Total: R$ 00,00</b>
+                  Data de solicitação da compra:  {dataPedido} <br />
+                  Produto R$ 00,00 {/*pedidoId*/} <br />
+                  Envio {formatPrice(valorFrete)} <br />
+                  <b>Total: {formatPrice(valorTotal)}</b>
                 </small>
               </ContentDetails>
             </CardDatailsContent>
