1328b56b88a42159c3079ffff0dd8e07b570399f
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2021-12-15 12:16:57 -0300

    adicionados a requisição e os dados da empresa

--- src/pages/Dashboard/PersonalData/index.tsx
+++ src/pages/Dashboard/PersonalData/index.tsx
@@ -1,8 +1,10 @@
 // import React from "react";
 // import Modal from "react-modal";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../../../components/Header";
 import { Menu } from "../../../components/Menu";
+import { api } from "../../../services/api";
 import {
   // CardProfile,
   // CardDatas,
@@ -13,6 +15,78 @@ import {
 } from "./styles";
 
 export default function PersonalData() {
+  const [nome, setNome]=useState('');  
+  const [marca, setMarca] = useState('')
+  const [razaoSocial, setRazaoSocial] = useState('')
+  const [cnpj, setCnpj] = useState('')
+  const [telefone, setTelefone] = useState('')
+  const [ramal, setRamal] = useState('')
+  const [email, setEmail] = useState('')
+  const [website, setWebsite] = useState('')
+  const [cep, setCep] = useState('')
+  const [logradouro, setLogradouro] = useState('')
+  const [numero, setNumero] = useState('')
+  const [complemento, setComplemento] = useState('')
+  const [pontoReferencia, setPontoReferencia] = useState('')
+  const [cidade, setCidade] = useState('')
+  const [estado, setEstado] = useState('')
+  const [bairro, setBairro] = useState('')
+  const [pix, setPix] = useState('')
+
+  const [newNome, setNewNome]=useState('');  
+  const [newMarca, setNewMarca] = useState('')
+  const [newRazaoSocial, setNewRazaoSocial] = useState('')
+  const [newCnpj, setNewCnpj] = useState('')
+  const [newTelefone, setNewTelefone] = useState('')
+  const [newRamal, setNewRamal] = useState('')
+  const [newEmail, setNewEmail] = useState('')
+  const [newWebsite, setNewWebsite] = useState('')
+  const [newCep, setNewCep] = useState('')
+  const [newLogradouro, setNewLogradouro] = useState('')
+  const [newNumero, setNewNumero] = useState('')
+  const [newComplemento, setNewComplemento] = useState('')
+  const [newPontoReferencia, setNewPontoReferencia] = useState('')
+  const [newCidade, setNewCidade] = useState('')
+  const [newEstado, setNewEstado] = useState('')
+  const [newBairro, setNewBairro] = useState('')
+  const [newPix, setNewPix] = useState('')
+
+
+
+
+    useEffect(
+      () => {
+        async function loadData(){          
+          const response = await api.get('empresa-perfil')
+          .then(response => {
+              console.log(response.data)
+              return response.data;            
+          })
+
+          setNome(response.nome)
+          setMarca(response.marca)
+          setRazaoSocial(response.razaoSocial)
+          setCnpj(response.cnpj)
+          setTelefone(response.telefone)
+          setRamal(response.ramal)
+          setEmail(response.email)
+          setWebsite(response.website)
+          setCep(response.cep)
+          setLogradouro(response.logradouro)
+          setNumero(response.numero)
+          setComplemento(response.complemento)
+          setPontoReferencia(response.pontoReferencia)
+          setCidade(response.cidade)
+          setEstado(response.estado)
+          setBairro(response.bairro)
+          setPix(response.pix)
+      
+        }
+        loadData()
+      }
+      , []
+    )
+
   return (
     <>
       <Header />
@@ -23,7 +97,7 @@ export default function PersonalData() {
           <CardDatailsContent>
             <ContentDetails>
               <span>Login:</span>
-              <p>Julia Souza</p>
+              <p>{email}</p>
             </ContentDetails>
           </CardDatailsContent>
 
@@ -37,40 +111,40 @@ export default function PersonalData() {
         </CardDatails>
 
         <CardDatails>
-          <h2>Empresa</h2>
+          <h2>{nome}</h2>
           <CardDatailsContent>
             <ContentDetails>
-              <span>Marca:</span>
+              <span>Marca: {marca}</span>
             </ContentDetails>
           </CardDatailsContent>
 
           <CardDatailsContent>
             <ContentDetails>
-              <span>Razão Social:</span>
+              <span>Razão Social: {razaoSocial}</span>
             </ContentDetails>
           </CardDatailsContent>
 
           <CardDatailsContent>
             <ContentDetails>
-              <span>CNPJ:</span>
+              <span>CNPJ: {cnpj} </span>
             </ContentDetails>
           </CardDatailsContent>
 
           <CardDatailsContent>
             <ContentDetails>
-              <span>Telefone:</span>
+              <span>Telefone: {telefone} </span>
             </ContentDetails>
           </CardDatailsContent>
 
           <CardDatailsContent>
             <ContentDetails>
-              <span>E-mail:</span>
+              <span>E-mail: {email} </span>
             </ContentDetails>
           </CardDatailsContent>
 
           <CardDatailsContent>
             <ContentDetails>
-              <span>Website:</span>
+              <span>Website: {website} </span>
             </ContentDetails>
           </CardDatailsContent>
         </CardDatails>
@@ -80,10 +154,10 @@ export default function PersonalData() {
           <CardDatailsContent className="adress">
             <ContentDetails>
               <small>
-                Av. xxxxxxxx <br />
-                Referência: XXXXXX <br />
-                CEP: 07355-620 <br />
-                Cidade: SP
+                Logradouro: {logradouro} <br />
+                Referência: {pontoReferencia} <br />
+                CEP: {cep} <br />
+                Cidade: {cidade}
               </small>
             </ContentDetails>
             <div className="flex-btn">
