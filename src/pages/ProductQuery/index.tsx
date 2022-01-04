a7f6d44551aab690d908765ac82602bc3ad9ad13
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-01-04 10:31:51 -0300

    adicionado um campo para mensagem de email e linkado este ultimo com o back

--- src/pages/ProductQuery/index.tsx
+++ src/pages/ProductQuery/index.tsx
@@ -5,13 +5,16 @@ import prodfav from "../../assets/images/prodfav.png"
 import { Link } from "react-router-dom"
 import { Menu } from "../../components/Menu"
 import { useEffect, useState } from "react"
-import { api, ip, role, status, tenantId } from "../../services/api"
+import { api, Email, ip, role, status, tenantId } from "../../services/api"
 import { Product } from "../../types"
 import axios from "axios"
 import { toast } from "react-toastify"
+import { ContentFormNew } from "../Profile/styles"
 
 function ProductQuery() {
   const [produtos = [], setProdutos] = useState<any[]>([]);
+  const [produtoRecusado = [], setProdutoRecusado] = useState<any>();
+  const [emailContent = [], setEmailContent] = useState<any>();
   const [loading, setLoading] = useState(false);
 
   async function loadUser() {
@@ -69,8 +72,23 @@ function ProductQuery() {
       console.log(response)
       if (response.statusText == "OK") {
         toast.info('Produto recusado com sucesso! :)');
-        //window.location.reload();
         loadUser()
+        console.log(response)
+        axios.post('http://'+ip+':8157/api/produto/enviarEmailRecusado', {
+          userId: produto.empresa.userId,
+          product: produto, 
+          emailContent: emailContent
+        }).then((response) => {
+          if(response.status == 200){
+            console.log(response)
+            toast.info('Email enviado com sucesso! :)')
+            setEmailContent('')
+          }
+          else{
+            console.log(response)
+            toast.info('Email não foi enviado com sucesso! :(')
+          }
+        })
       }else{
         toast.error('Ops, não foi possivel recusado o produto! :(');
       }
@@ -89,6 +107,7 @@ function ProductQuery() {
          
           {
             produtos.map((produto) => (
+              <>
               <S.CardDatailsContent>
                 <S.ContentDetails>
                   <img src={produto.imagemUrl} alt="" />
@@ -101,6 +120,17 @@ function ProductQuery() {
                   <Link to="/consultar-produtos" onClick={() => recusarProd(produto)}>Recusar</Link>
                 </S.BtnContent>
               </S.CardDatailsContent>
+                <ContentFormNew 
+                style={{backgroundColor: 'white', padding: 15}}>
+                  <label htmlFor="">Se caso recusado, por quê?</label>
+                  <textarea
+                    required
+                    placeholder="Resposta"
+                    onChange={(text) => setEmailContent(text.target.value)}
+                  // value="5165161"
+                  />
+                </ContentFormNew>
+              </>
             ))
 
           }
