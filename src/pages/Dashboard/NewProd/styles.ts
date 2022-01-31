8d03710aac0ed128b3abdd03cde0812f6868e4bb
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-01-31 13:45:54 -0300

    arrumada a estilização da descrição

--- src/pages/Dashboard/NewProd/styles.ts
+++ src/pages/Dashboard/NewProd/styles.ts
@@ -77,6 +77,10 @@ export const ProdContainerSingle = styled.div`
     overflow-wrap: anywhere;
     font-size: 12px;
     padding: 6px 7px;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
     @media (max-width: 768px) {
       font-size: 14px;
     }
