dceb90bf7e9ec563ab582658ac2a7a5d397f4ac0
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-02-10 16:03:30 -0300

    reestruturado o style

--- src/pages/Dashboard/Questions/styles.ts
+++ src/pages/Dashboard/Questions/styles.ts
@@ -102,4 +102,18 @@ export const TextAreaFormated = styled.textarea`
   -moz-border-radius-bottomleft: 5px;
   border-bottom-right-radius: 5px;
   border-bottom-left-radius: 5px;
+  transform: translateY(-5px);
+`
+
+
+
+export const CardDatailsContentSecondary = styled.div`
+  background: white;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 12px;
+  transform: translateY(-5px);
 `
