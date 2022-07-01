c9bb4581f17a6c6112160b26b5fcb13f681bd069
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-01 12:30:43 -0300

    fixed the page's broken styles

--- src/pages/Dashboard/SendBanner/styles.ts
+++ src/pages/Dashboard/SendBanner/styles.ts
@@ -46,7 +46,7 @@ export const CardDatails = styled.div`
   h2 {
     color: #000;
     font-size: 30px;
-    padding: 40px 0 30px 0;
+    padding: 0 0 30px 0;
   }
 
   .emailSender{
@@ -74,14 +74,51 @@ export const CardDatails = styled.div`
     border-radius: 5px;
   } */
 
+  .specifications{
+    padding: 1rem 0.9rem;
+    margin-bottom: 0;
+  }
+
+  p{
+    alighn-self: center;
+  }
+
+
+  .btn-info {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 210px;
+    height: 41px;
+    text-decoration: none;
+    background: black;
+    color: white;
+    border-radius: 5px;
+    cursor: pointer;
+    margin-right: .5rem;
+  } 
+
+  .flex-container{
+    display: flex;
+    alighn-items: center;
+  }
+
+  .upload-input{
+    align-self: center;
+  }
+
+
+
+
   .file {
     background: white;
     width: 100%;
-    height: 250px;
+    
+    min-height: 250px;
     margin: 20px 0;
     display: flex;
     flex-direction: column;
-    padding: 0 12px;
+    padding: 5%;
     border-radius: 5px;
 
     input[type=submit] {
@@ -91,11 +128,27 @@ export const CardDatails = styled.div`
       color: #FFF;
       text-align: center;
       display: block;
+      /*
       margin-top: 10px;
+      */
       cursor: pointer;
       border: 0;
       border-radius: 5px;
     } 
+
+
+
+    input[type=file]::-webkit-file-upload-button {
+      visibility: hidden;
+      display: none;
+    }
+
+    input[type=file]::file-selector-button {
+      display: none;
+    }
+
+    
+
   }
 `
 
@@ -174,6 +227,8 @@ export const CardDatailsContent = styled.div`
 export const ContentDetails = styled.div`
   display: flex;
   align-items: center;
+  width: 125%;
+  justify-content: space-between;
 
   span {
     font-size: 18px;
@@ -184,8 +239,8 @@ export const ContentDetails = styled.div`
     padding: 0 40px;
   }
 
-  p {
-    margin-left: 12px;
+  .reset-btn{
+    margin:0; 
   }
   
   small {
