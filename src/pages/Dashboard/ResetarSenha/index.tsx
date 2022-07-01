9e4506e5d99ac20f0fef9bd5a8a96ab08b8aa8b2
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-01 11:19:54 -0300

    fixed all localstorage variables

--- src/pages/Dashboard/ResetarSenha/index.tsx
+++ src/pages/Dashboard/ResetarSenha/index.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 // import Modal from "react-modal";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import Header from "../../../components/Header";
@@ -29,6 +29,8 @@ export default function ResetarSenha() {
   const [showModal2, setShowModal2] = React.useState(false);
   const [email              , setEmail              ] = useState('')
   const [userId             , setuserId             ] = useState('')
+  const [userToken             , setUserToken             ] = useState('')
+  const [tenantId             , setTenantId             ] = useState('')
   const [senha              , setSenha              ] = useState("");
   const [showModalResetSenha, setShowModalResetSenha] = useState(false);
   const [loading            , setLoading            ] = useState(false);
@@ -37,25 +39,43 @@ export default function ResetarSenha() {
     // references are now sync'd and can be accessed.
   }
 
+  useLayoutEffect(() => {
+    console.log("window.location.hash")
+    console.log(window.location.hash)
+    // const hash = window.location.hash.replace(`#/resetar-senha/`, ``);
+    const hash = window.location.href.split(`#/resetar-senha/`)[1]
+    localStorage.setItem("token", JSON.stringify(hash));
+  }
+  
+  , []
+  ) 
+
 
   useEffect(() => {
 
     console.log("window.location.hash")
     console.log(window.location.hash)
 
-    const hash = window.location.hash.replace(`#/resetar-senha/`, ``);
+    // const hash = window.location.hash.replace(`#/resetar-senha/`, ``);
+    const hash = window.location.href.split(`#/resetar-senha/`)[1]
+
+
+    console.log("hash")
+    setUserToken(hash)
 
     if (hash) {
       
-      localStorage.setItem("token", JSON.stringify(hash));
       loadUser(hash)
-
+      
     }
   }
-
-    , []
+  
+  , []
   )
   async function loadUser(token: string) {
+    console.log("token")
+    console.log(token)
+    
     if (!token) {
       //window.location.reload()
     }
@@ -75,6 +95,7 @@ export default function ResetarSenha() {
     //// console.log(response);
     //// console.log(response.tenants[0].roles[0]);
     let setRole = response.tenants[0].roles
+    localStorage.setItem("token", JSON.stringify(token));
     const roleHelper = JSON.parse(setRole)
     // console.log(roleHelper[0])
     localStorage.setItem("roles", JSON.stringify(roleHelper[0])); //saves client's data into localStorage:
@@ -84,17 +105,30 @@ export default function ResetarSenha() {
       "tenantId",
       JSON.stringify(response.tenants[0].tenant.id)
     ); //saves client's data into localStorage:
+
+    setTenantId(response.tenants[0].tenant.id)
     localStorage.setItem("id", JSON.stringify(response.id)); //saves client's data into localStorage:
     localStorage.setItem("status", JSON.stringify(response.tenants[0].status)); //saves client's data into localStorage:
     localStorage.setItem("email", JSON.stringify(response.email));
+    setEmail(response.email);
+
+
 
     setuserId(response.id)
   }
 
   async function resetSenha() {
     setLoading(true)
+
+    const api = axios.create({
+      baseURL: ip+':8157/api/tenant/'+tenantId +"/",
+      // baseURL: ''+ip+':8157/api/tenant/'+tenantId || "fa22705e-cf27-41d0-bebf-9a6ab52948c4" +"/",
+      timeout: 50000,
+      headers: {'Authorization': 'Bearer '+ userToken}
+    });
+    
   
-    await api.get("user/" + id).then((response) => {
+    await api.get("user/" + userId).then((response) => {
       update(response.data)
       return response.data;
     });
